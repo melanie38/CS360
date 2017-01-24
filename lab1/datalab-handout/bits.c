9e6bb85b66ab54bc92ae9ad14104ef2ba8584709
Alex Chu <alexechu1@gmail.com>
2017-01-23 23:53:50 -0700

    first attempt at implemented bitcount

--- lab1/datalab-handout/bits.c
+++ lab1/datalab-handout/bits.c
@@ -216,7 +216,10 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+  int i = x;
+  i = i - ((i >> 1) & 0x55555555);
+  i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+  return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
 }
 /* 
  * bang - Compute !x without using !
